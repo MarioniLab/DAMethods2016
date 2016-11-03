5ce7b8f734999beabc320f802d8fb3fc903110fc
Aaron Lun <alun@wehi.edu.au>
2016-11-03 09:58:26 +0000

    Updated, added preprocessing script for BMMC data.

--- refdata/preprocess_44185.R
+++ refdata/preprocess_44185.R
@@ -1,30 +1,27 @@
-# This performs logicle-transformation and gating on intensities for each data set. 
+#This performs logicle-transformation and gating on intensities for each data set. 
 
 library(ncdfFlow)
 host.dir <- "."
-for (dataset in 1:3) {
-    ref.dir <- "Cytobank_43324"
-    if (dataset==1L) {
-        extra <- "4FI"
-    } else if (dataset==2L) {
-        extra <- "NG"
-    } else if (dataset==3L) {
-        extra <- "NN"
-    }
+for (dataset in 1:5) {
+    ref.dir <- "Cytobank_44185"
+    extra <- paste0("H", dataset)
+
     ref.out.dir <- paste0(ref.dir, "_", extra)
-    pattern <- sprintf("^%s_[0-9][0-9].fcs", extra)
+    pattern <- sprintf("^%s_.*", extra)
     
     # Reading in the files.
     fcs <- list.files(file.path(host.dir, ref.dir), full=TRUE, pattern=pattern)
     nfs <- read.ncdfFlowSet(fcs[1])
-    toignore <- c(match(c("Cell_length", "Time", "beadDist", "barcode"), colnames(nfs)), grep("^BC-[0-9]", colnames(nfs)))
+    descriptions <- as.character(parameters(nfs[[1]])$desc)
+    toignore <- c(match(c("Time", "Cell_length", "PhenoGraph"), descriptions), grep("^BC[0-9]", descriptions))
     
-    # Gate specifications.
-    gate.up <- match(c("DNA-1(Ir191)Dd", "DNA-2(Ir193)Dd"), colnames(nfs))
-    gate.down <- NULL
+    # Choosing which ones to gate on.
+    gate.up <- match(c("DNA1", "DNA2"), descriptions)
+    gate.down <- match(c("Viability"), descriptions)
 
     ########################################################
     # Estimating the parameters of the logicle transformation, after pooling all samples.
+    
     new.exprs <- list()
     new.nfs <- suppressWarnings(suppressMessages(read.ncdfFlowSet(fcs)))
     for (i in seq_along(fcs)) {
@@ -36,17 +33,22 @@ for (dataset in 1:3) {
     parameters(replaced.ff)$minRange <- apply(new.exprs, 2, min)
     parameters(replaced.ff)$maxRange <- apply(new.exprs, 2, max)
     parameters(replaced.ff)$range <- parameters(replaced.ff)$maxRange - pmax(parameters(replaced.ff)$minRange, 0)
-    lgcl <- estimateLogicle(replaced.ff, channels=colnames(replaced.ff)[-toignore], type='data')
+    lgcl <- estimateLogicle(replaced.ff, channels=colnames(replaced.ff)[-toignore], type='data', m=5) # m=5 to avoid error.
     trans.ff <- transform(replaced.ff, lgcl)
     trans.nfs <- transform(new.nfs, lgcl)
    
     ########################################################
     # Need to gate on specified aspects (gate.up, gate.down).
 
-    med.per.gate <- apply(exprs(trans.ff)[,gate.up], 2, median) # Using the median and MAD across _all_ events.
-    mad.per.gate <- apply(exprs(trans.ff)[,gate.up], 2, mad)
+    all.gates <- c(gate.up, gate.down)
+    threshold.above <- rep(c(TRUE, FALSE), c(length(gate.up), length(gate.down)))
+    med.per.gate <- apply(exprs(trans.ff)[,all.gates], 2, median) # Using the median and MAD across _all_ events.
+    mad.per.gate <- apply(exprs(trans.ff)[,all.gates], 2, mad)
     lower.bound <- med.per.gate - 3 * mad.per.gate
-    toignore <- c(toignore, gate.up)
+    lower.bound[!threshold.above] <- -Inf
+    upper.bound <- med.per.gate + 3 * mad.per.gate
+    upper.bound[threshold.above] <- Inf
+    toignore <- c(toignore, all.gates)
 
     # Running through the files, subsetting by the specified gates, and saving the FCS files.
     # This allows us to use the exact same transformed intensities for tools that requires FCS input.
@@ -54,11 +56,10 @@ for (dataset in 1:3) {
     fcs.dir <- file.path(ref.out.dir, "fcs") 
     dir.create(fcs.dir)
     saved <- list()
-
     for (x in sampleNames(trans.nfs)) {
         cur.fcs <- trans.nfs[[x]]
-        tex <- t(exprs(cur.fcs)[,gate.up,drop=FALSE])
-        stats <- tex < lower.bound 
+        tex <- t(exprs(cur.fcs)[,all.gates,drop=FALSE])
+        stats <- tex < lower.bound | tex > upper.bound
         saved[[x]] <- rowMeans(stats)
         keep <- colSums(stats)==0L
         cur.fcs <- cur.fcs[keep,]
